6f0d826990aa45d07fe885af7f31231772d85a8b
nitishaadhikari <nitisha20adhikari@gmail.com>
2015-12-28 02:16:04 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should retrieve the 
+## inverse from the cache
 
-## Write a short comment describing this function
+## makeCacheMatrix will create a matrix x, and expose three methods to set/get x and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+cachedInv <- NULL ## initialize inverse
+  
+  ## set x in parent env with the desired value, if inverse is already set, get rid of it!
+  set <- function(userValue = matrix()) {
+    x <<- userValue 
+    cachedInv <<- NULL
 }
 
+get <- function() x
+  
+  ##set inverse variable in parent env to desired value and return the value as a convenience
+  setInverse <- function(invVal) {
+    cachedInv <<- invVal 
+    return(cachedInv)
+  }
+  
+  getInverse  <- function() cachedInv
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
+}
 
-## Write a short comment describing this function
+## given the list variable from the first function, will first check to see if there's already a cached inverse and return
+## otherwise will attempt to solve its inverse and set/return it
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x = makeCacheMatrix(1:4, nrow=2, ncol=2), ...)  {
         ## Return a matrix that is the inverse of 'x'
+        calculatedInverse <- x$getInverse() 
+  
+  ##check if there's a cached value AND it's a matrix
+  if(!is.null(calculatedInverse) && is.matrix(calculatedInverse)) { 
+    message("We found cached data and saved valuable cpus!!!")
+    return(calculatedInverse)
+  }
+  
+  ## otherwise get the matrix
+  matrixToSolve <- x$get()  
+  
+  ## try to solve the matrix and catch errors and warnings
+  calculatedInverse <- tryCatch({ 
+    solve(matrixToSolve)
+  }, warning=function(w) {
+    message("This may not be the result you're looking for")
+    message(w)
+  }, error=function(e) {
+    message("Something went wrong solving your matrix")
+    message(e)
+    message("\n")
+  })
+  
+  ## whatever the case, set the value of the inverse (NULL if something went wrong)
+  message("Setting the value of inverse to:") 
+  x$setInverse(calculatedInverse)
 }
+
